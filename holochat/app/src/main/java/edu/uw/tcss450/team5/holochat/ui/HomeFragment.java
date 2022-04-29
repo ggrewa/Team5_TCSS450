bfe31c131ecf01b13507a280bc42f44ed9325954
Gurleen Grewal <ggrewa@uw.edu>
2022-04-28 19:22:59 -0700

    fix nav issue and MainActivity args null pointer issue

--- holochat/app/src/main/java/edu/uw/tcss450/team5/holochat/ui/HomeFragment.java
+++ holochat/app/src/main/java/edu/uw/tcss450/team5/holochat/ui/HomeFragment.java
@@ -2,13 +2,18 @@ package edu.uw.tcss450.team5.holochat.ui;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.lifecycle.ViewModelProvider;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import edu.uw.tcss450.team5.holochat.R;
+import edu.uw.tcss450.team5.holochat.databinding.FragmentHomeBinding;
+import edu.uw.tcss450.team5.holochat.model.UserInfoViewModel;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -23,5 +28,14 @@ public class HomeFragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_home, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        UserInfoViewModel model = new ViewModelProvider(getActivity())
+                .get(UserInfoViewModel.class);
+
+        FragmentHomeBinding.bind(getView()).textHello.setText("Hello " + model.getEmail());
     }
 }
