0f1368d47f97019ef57d4dc0a6527ada1e7438af
Ken Ahrens <75004340+Kenpai718@users.noreply.github.com>
2022-05-01 23:02:42 -0700

    Setting up initial contact fragments
    just fragment class creation no coding yet

---
+++ holochat/app/src/main/java/edu/uw/tcss450/team5/holochat/ui/contacts/ContactListRecyclerView.java
@@ -0,0 +1,4 @@
+package edu.uw.tcss450.team5.holochat.ui.contacts;
+
+public class ContactListRecyclerView {
+}
---
+++ holochat/app/src/main/java/edu/uw/tcss450/team5/holochat/ui/contacts/ContactListViewModel.java
@@ -0,0 +1,7 @@
+package edu.uw.tcss450.team5.holochat.ui.contacts;
+
+import androidx.lifecycle.ViewModel;
+
+public class ContactListViewModel extends ViewModel {
+    // TODO: Implement the ViewModel
+}
