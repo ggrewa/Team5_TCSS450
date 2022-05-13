6aaef92fa05f9157ec3149429d8af70833822cbb
Mason Hansen <masonh6@uw.edu>
2022-05-13 13:24:29 -0700

    enter the name of a city and get weather stats!!!

--- holochat/app/src/main/java/edu/uw/tcss450/team5/holochat/ui/WeatherFragment.java
+++ holochat/app/src/main/java/edu/uw/tcss450/team5/holochat/ui/WeatherFragment.java
@@ -2,23 +2,129 @@ package edu.uw.tcss450.team5.holochat.ui;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+import com.google.android.material.tabs.TabLayoutMediator;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+import org.w3c.dom.Text;
+
+import java.text.DecimalFormat;
 
 import edu.uw.tcss450.team5.holochat.R;
+import edu.uw.tcss450.team5.holochat.ui.contacts.contact_tabs.ContactTabsAdapter;
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class WeatherFragment extends Fragment {
 
+    //textbox where the user can enter the city
+    EditText etCity;
+    //button to refresh results
+    Button btnget;
+    //text result of the weather
+    TextView tvResult;
+
+    private final String url = "https://api.openweathermap.org/data/2.5/weather";
+    private final String appid = "5d35717e8f7700ac945b1abc468129d0";
+
+    DecimalFormat df = new DecimalFormat("#.##");
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
+        /*
+        View view = inflater.inflate(R.layout.fragment_weather, null, false);
+        TextView tvResult = (TextView) view.findViewById(R.id.tvResult);
+        return view;
+        */
         return inflater.inflate(R.layout.fragment_weather, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        etCity = view.findViewById(R.id.etCity);
+        btnget = view.findViewById(R.id.btnget);
+        btnget.setOnClickListener(this::getWeatherDetails);
+        tvResult = view.findViewById(R.id.tvResult);
+    }
+
+    public void getWeatherDetails(View view){
+        String tempUrl = "";
+        String city = etCity.getText().toString().trim();
+        if(city.equals("")){
+            tvResult.setText("bro you forgot to enter the city");
+        } else {
+            tempUrl = url + "?q=" + city + "&appid=" + appid;
+        }
+        StringRequest stringRequest = new StringRequest(Request.Method.POST, tempUrl, new Response.Listener<String>() {
+            @Override
+            public void onResponse(String response) {
+                //Log.d("response", response);
+                String output = "";
+                try {
+                    JSONObject jsonResponse = new JSONObject(response);
+                    JSONArray jsonArray = jsonResponse.getJSONArray("weather");
+                    JSONObject jsonObjectWeather = jsonArray.getJSONObject(0);
+                    String description = jsonObjectWeather.getString("description");
+                    JSONObject jsonObjectMain = jsonResponse.getJSONObject("main");
+                    //use for °C
+                    double temp = jsonObjectMain.getDouble("temp") - 273.15;
+                    double feelsLike = jsonObjectMain.getDouble("feels_like") - 273.15;
+                    //use for °F
+                    double tempF = 1.8 * (jsonObjectMain.getDouble("temp") - 273.15) +32;
+                    double feelsLikeF = 1.8 * (jsonObjectMain.getDouble("feels_like") - 273.15) +32;
+                    float pressure = jsonObjectMain.getInt("pressure");
+                    int humidity = jsonObjectMain.getInt("humidity");
+                    JSONObject jsonObjectWind = jsonResponse.getJSONObject("wind");
+                    String wind = jsonObjectWind.getString("speed");
+                    JSONObject jsonObjectClouds = jsonResponse.getJSONObject("clouds");
+                    String clouds = jsonObjectClouds.getString("all");
+                    JSONObject jsonObjectSys = jsonResponse.getJSONObject("sys");
+                    String countryName = jsonObjectSys.getString("country");
+                    String cityName = jsonResponse.getString("name");
+
+                    output += "Current weather of " + cityName + " (" + countryName + ")"
+                            + "\n Temp: " + df.format(tempF) + " °F"
+                            + "\n Feels Like: " + df.format(feelsLikeF) + " °F"
+                            + "\n Humidity: " + humidity + "%"
+                            + "\n Description: " + description
+                            + "\n Wind Speed: " + wind + "m/s (meters per second)"
+                            + "\n Cloudiness: " + clouds + "%"
+                            + "\n Pressure: " + pressure + " hPa";
+                    tvResult.setText(output);
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+            }
+        }, new Response.ErrorListener(){
+                @Override
+                public void onErrorResponse(VolleyError error){
+                    Toast.makeText(getActivity(), error.toString().trim(), Toast.LENGTH_SHORT).show();
+                }
+        });
+
+        RequestQueue requestQueue = Volley.newRequestQueue(getActivity());
+        requestQueue.add(stringRequest);
     }
 }
