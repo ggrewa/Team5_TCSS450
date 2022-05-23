a93c7e5617fd1b110380470031d0ad6ba7628fec
Ken Ahrens <75004340+Kenpai718@users.noreply.github.com>
2022-05-23 11:05:19 -0700

    Added Dark mode to authactivity
    -added theme saving to authactivity
    -changed some dark mode colors so they are less bright
    -made change password a different color to follow HCI principles
    -change auth xml files to work with night mode

--- holochat/app/src/main/java/edu/uw/tcss450/team5/holochat/AuthActivity.java
+++ holochat/app/src/main/java/edu/uw/tcss450/team5/holochat/AuthActivity.java
@@ -11,6 +11,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.lifecycle.ViewModelProvider;
 
 import edu.uw.tcss450.team5.holochat.model.PushyTokenViewModel;
+import edu.uw.tcss450.team5.holochat.utils.AppSharedPref;
 import me.pushy.sdk.Pushy;
 
 /*
@@ -21,14 +22,19 @@ import me.pushy.sdk.Pushy;
  */
 public class AuthActivity extends AppCompatActivity {
 
+    AppSharedPref mPref;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_auth);
 
+        // start of theme implementation. Sets theme at start of app run based on settings.
+        mPref = new AppSharedPref(this);
+        mPref.initializeTheme(); //set theme based on preference in AppSharedPref class
+
         //If it is not already running, start the Pushy listening service
         Pushy.listen(this);
-
         initiatePushyTokenRequest();
     }
 
