b4a8eb988e205b7567b84fb30494f75e66b86d25
Tarnveer Mangat <tarnveermangat@Tarnveers-MacBook-Pro.local>
2022-05-31 19:34:26 -0700

    contact
    fixed the xml from tarnveer push

--- holochat/app/src/main/java/edu/uw/tcss450/team5/holochat/ui/contacts/contact_tabs/ContactRequestListFragment.java
+++ holochat/app/src/main/java/edu/uw/tcss450/team5/holochat/ui/contacts/contact_tabs/ContactRequestListFragment.java
@@ -2,18 +2,27 @@ package edu.uw.tcss450.team5.holochat.ui.contacts.contact_tabs;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.lifecycle.ViewModelProvider;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import edu.uw.tcss450.team5.holochat.R;
+import edu.uw.tcss450.team5.holochat.databinding.FragmentContactRequestBinding;
+import edu.uw.tcss450.team5.holochat.model.UserInfoViewModel;
 
 /**
  * Fragment that holds all friend requests the user has recieved
+ * @author Tarnveer
  */
 public class ContactRequestListFragment extends Fragment {
+    private ContactRequestListViewModel mModel;
+    FragmentContactRequestBinding binding;
 
     public ContactRequestListFragment() {
         // Required empty public constructor
@@ -22,6 +31,13 @@ public class ContactRequestListFragment extends Fragment {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        mModel = new ViewModelProvider(getActivity()).get(ContactRequestListViewModel.class);
+
+        UserInfoViewModel model = new ViewModelProvider(getActivity())
+                .get(UserInfoViewModel.class);
+
+        Log.i("CONTACT", model.getJwt());
+        mModel.connectGet(model.getJwt());
     }
 
     @Override
@@ -29,5 +45,23 @@ public class ContactRequestListFragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_contact_request, container, false);
+    }
+    /**
+     * Creates an instance of the fragment for when the user returns to it.
+     */
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        //Local access to the ViewBinding object. No need to create as Instance Var as it is only
+        //used here.
+        FragmentContactRequestBinding binding = FragmentContactRequestBinding.bind(getView());
+
+        mModel.addContactRequestListObserver(getViewLifecycleOwner(), contactList -> {
+            //if (!contactList.isEmpty()) {
+            binding.listRoot.setAdapter(
+                    new ContactRequestRecyclerViewAdapter(contactList, getActivity().getSupportFragmentManager())
+            );
+        });
     }
 }
