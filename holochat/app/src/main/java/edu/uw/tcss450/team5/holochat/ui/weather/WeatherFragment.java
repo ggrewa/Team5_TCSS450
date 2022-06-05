748bc880ce6e9cd7a291dfd0e53f69a880655006
Ken Ahrens <75004340+Kenpai718@users.noreply.github.com>
2022-06-05 13:13:05 -0700

    unhardcoded weather calls

--- holochat/app/src/main/java/edu/uw/tcss450/team5/holochat/ui/weather/WeatherFragment.java
+++ holochat/app/src/main/java/edu/uw/tcss450/team5/holochat/ui/weather/WeatherFragment.java
@@ -270,7 +270,7 @@ public class WeatherFragment extends Fragment {
      * @param zip
      */
     public void connect(String zip) {
-        String webServiceUrl = "https://team5-tcss450-holochat.herokuapp.com/weather/" + zip;
+        String webServiceUrl = getResources().getString(R.string.base_url_service)  + "weather/" + zip;
         Request request = new JsonObjectRequest(
                 Request.Method.GET,
                 webServiceUrl,
